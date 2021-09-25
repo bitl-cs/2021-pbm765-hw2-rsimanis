aaf0015ae7950afbf4553b2d6f38bc84ca5b7f6f
rsimanis <raivis.simanis@gmail.com>
2021-09-25 18:23:36 +0300

    Task1 fixed + c90 compliance

--- hw2.c
+++ hw2.c
@@ -5,53 +5,55 @@
 #define	MAX_LEN	256
 
 int read_line(int fd, char* buffer);
-int validate_keyword(FILE* fp, int len, char* ending);
+int validate_keyword(int fd, int len, char* ending);
 
 int main()
 {
 	char name[MAX_LEN];
+	int kw;
 
-	int kw = read_line(STDIN_FILENO, name);
-	printf("%d\n", kw);
-	if (kw >= 0)
+	while ((kw = read_line(STDIN_FILENO, name)) >= 0) {
+		printf("%d\n", kw);
 		printf("%s\n", name);	
+	}
+	printf("%d\n", kw);
 	return 0;
 }
 
 int read_line(int fd, char* buffer)
 {
-	FILE* fp;
-	int c, i, kw, ws;
+	char c;
+	int i, kw, ws;
 
-	fp = fdopen(fd, "r");
-	c = fgetc(fp);
-	if (c == '\n' || c == EOF)
+	if (read(fd, &c, 1) == 0)
 		return -1;
+	while (c == '\n')
+		if (read(fd, &c, 1) == 0)
+			return -1;
+
 	if (c == 'N') {
-		if (!validate_keyword(fp, 4, "AME "))
+		if (!validate_keyword(fd, 4, "AME "))
 			return -2;
 		kw = 0;
 	}
 	else if (c == 'M') {
-		if (!validate_keyword(fp, 6, "OTHER "))
+		if (!validate_keyword(fd, 6, "OTHER "))
 			return -2;
 		kw = 1;
 	}
 	else if (c == 'F') {
-		if (!validate_keyword(fp, 6, "ATHER "))
+		if (!validate_keyword(fd, 6, "ATHER "))
 			return -2;
 		kw = 2;
 	}
 	else
 		return -2;
 
-	c = fgetc(fp);
-	if (c == ' ' || c == '\t')
+	if (read(fd, &c, 1) == 0 || c == '\n' || c == ' ' || c == '\t')
 		return -3;
-	i = 0;
 	ws = 0;
-	buffer[i] = c;
-	for (i = 1; (c = fgetc(fp)) != EOF && c != '\n'; i++) {
+	buffer[0] = c;
+	for (i = 1; read(fd, &c, 1) != 0 && c != '\n'; i++) {
 		if (c == ' ') {
 			ws++;
 			if (ws > 1)
@@ -68,21 +70,15 @@ int read_line(int fd, char* buffer)
 	return kw;
 }
 
-int validate_keyword(FILE* fp, int len, char* ending)
+int validate_keyword(int fd, int len, char* ending)
 {
 	int c, i;
 	char temp[7];
 
-	for (i = 0; i < len && (c = fgetc(fp)) != EOF && c != '\n'; i++)
+	for (i = 0; i < len && read(fd, &c, 1) != 0 && c != '\n'; i++)
 		temp[i] = c;
 	temp[i] = '\0';
 	if (i < len || strncmp(temp, ending, len) != 0)
 		return 0;
 	return 1;
 }
-
-
-
-
-
-
