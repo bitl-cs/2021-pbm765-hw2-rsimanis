9a589a3edc226d6a7feff4bf861fd48f8cd37823
rsimanis <raivis.simanis@gmail.com>
2021-09-29 01:28:24 +0300

    Fixed misalignments in person_lists.h

--- person_lists.h
+++ person_lists.h
@@ -5,18 +5,18 @@
 #define	MAX_LINE	256
 
 /* error codes */
-#define		E_INVALID_KEYWORD				-2
-#define		E_INVALID_NAME					-3
-#define		E_PARRENT_LIMIT					-4
-#define		E_PARRENT_GENERATION			-5
-#define		E_PARRENT_ABOVE					-6
-#define		E_INVALID_RECORD_START			-7
-#define		E_MERGE_ARGUMENTS				-8
-#define		E_INSERT_PERSON_ARGUMENTS		-9
-#define		E_INSERT_LIST_ARGUMENTS			-10
-#define		E_INSERT_LIST_FULL				-11
-#define		E_LOOPS							-12
-#define		E_PARRENT_TWO_GENDERS			-13
+#define		E_INVALID_KEYWORD		-2
+#define		E_INVALID_NAME			-3
+#define		E_PARRENT_LIMIT			-4
+#define		E_PARRENT_GENERATION		-5
+#define		E_PARRENT_ABOVE			-6
+#define		E_INVALID_RECORD_START		-7
+#define		E_MERGE_ARGUMENTS		-8
+#define		E_INSERT_PERSON_ARGUMENTS	-9
+#define		E_INSERT_LIST_ARGUMENTS		-10
+#define		E_INSERT_LIST_FULL		-11
+#define		E_LOOPS				-12
+#define		E_PARRENT_TWO_GENDERS		-13
 
 
 typedef struct _Person {
